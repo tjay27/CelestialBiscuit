51b9bc2e4666e343643911ae626b4c8e62bdf5d0
tjay27 <tejaswigurram27@gmail.com>
2022-12-26 18:42:24 +0530

    added more gallery pics

--- src/Components/PhotoGrid.js
+++ src/Components/PhotoGrid.js
@@ -37,6 +37,96 @@ const PhotoGrid = () => {
                     <div className='big-main img10'></div>
                 </div>
             </div>
+            <div className='row-main'>
+                <div className='column-main'>
+                    <div className='big-main img11'></div>
+                </div>
+                <div className='column-main'>
+                    <div className='row'>
+                        <div className='big img12'></div>
+                        <div className='small img13'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='small img14'></div>
+                        <div className='big img15'></div>
+                    </div>
+                </div>
+            </div>
+            <div className='row-main'>
+                <div className='column-main'>
+                    <div className='row'>
+                        <div className='big img16'></div>
+                        <div className='small img17'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='small img18'></div>
+                        <div className='big img19'></div>
+                    </div>
+                </div>
+                <div className='column-main'>
+                    <div className='big-main img20'></div>
+                </div>
+            </div>
+            <div className='row-main'>
+                <div className='column-main'>
+                    <div className='big-main img21'></div>
+                </div>
+                <div className='column-main'>
+                    <div className='row'>
+                        <div className='big img22'></div>
+                        <div className='small img23'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='small img24'></div>
+                        <div className='big img25'></div>
+                    </div>
+                </div>
+            </div>
+            <div className='row-main'>
+                <div className='column-main'>
+                    <div className='row'>
+                        <div className='big img26'></div>
+                        <div className='small img27'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='small img28'></div>
+                        <div className='big img29'></div>
+                    </div>
+                </div>
+                <div className='column-main'>
+                    <div className='big-main img30'></div>
+                </div>
+            </div>
+            <div className='row-main'>
+                <div className='column-main'>
+                    <div className='big-main img31'></div>
+                </div>
+                <div className='column-main'>
+                    <div className='row'>
+                        <div className='big img32'></div>
+                        <div className='small img33'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='small img34'></div>
+                        <div className='big img35'></div>
+                    </div>
+                </div>
+            </div>
+            <div className='row-main'>
+                <div className='column-main'>
+                    <div className='row'>
+                        <div className='big img36'></div>
+                        <div className='small img37'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='small img38'></div>
+                        <div className='big img39'></div>
+                    </div>
+                </div>
+                <div className='column-main'>
+                    <div className='big-main img40'></div>
+                </div>
+            </div>
         </div>
     </div>
     </div>
